705dc6d0670cc07baecb3f1d9e87441d44223d67
MiaHV20 <mia.hurtado@utec.edu.pe>
2025-09-24 07:38:11 -0500

    Add files via upload

---
+++ ingesta.py
@@ -0,0 +1,10 @@
+import boto3
+
+ficheroUpload = "data.csv"
+nombreBucket = "miahv20storage"
+
+s3 = boto3.client('s3')
+response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+print(response)
+
+print("Ingesta completada")
